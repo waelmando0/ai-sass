4e7b391101c78bcd9c2865873db0a6bd78732283
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2023-10-17 20:09:21 +0300

    testing auth

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const LandingPage = () => {
   return (
     <section className="container">
-      <div className="py-6">Landing Page (Unprotected)</div>
+      <div className="py-6">
+        Landing Page (Unprotected)
+        <div className="flex gap-x-4">
+          <div>
+            <Link href="/sign-in">
+              <Button>Login</Button>
+            </Link>
+          </div>
+          <div>
+            <Link href="/sign-up">
+              <Button>register</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
